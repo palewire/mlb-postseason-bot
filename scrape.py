95490b7ea085dd43dc75f3a408e41dc2e7e82469
palewire <b@palewi.re>
2022-04-02 15:16:53 -0700

    docstrings

--- scrape.py
+++ scrape.py
@@ -15,7 +15,9 @@ def cli():
 def fangraphs():
     """Scrape playoff odds from fangraphs."""
     with sync_playwright() as p:
+        # Open the browser
         browser_obj = p.chromium.launch()
+
         # Get the page
         page = browser_obj.new_page()
         page.goto("https://www.fangraphs.com/standings/playoff-odds")
@@ -23,15 +25,24 @@ def fangraphs():
         # Pull the html
         html = page.content()
 
+    # Parse the HTML
     soup = BeautifulSoup(html, "html5lib")
+
+    # Pull out the tables
     table_list = soup.find_all("table", class_="playoff-odds-table")
     team_dict = {}
+
+    # Loop through them
     for table in table_list:
         tbody = table.tbody
         tr_list = tbody.find_all("tr")
+        # Go through the rows
         for row in tr_list:
+            # Pull out the column want
             name = row.find("span", class_="fullName").text
             team_dict[name] = float(row.find("td", class_="highlight-sort").text.replace("%", ""))
+
+    # Write it out
     with open("./data/fangraphs.json", "w") as fp:
         json.dump(team_dict, fp, indent=2, sort_keys=True)
 
